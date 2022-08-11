f5044252c70167ba04b9c576d53adfa05ca0c57c
devninja309 <ryooyama.dev@gmail.com>
2022-08-12 03:00:42 +0900

    change refresh icon

--- src/page/home.tsx
+++ src/page/home.tsx
@@ -1,9 +1,17 @@
 import { Container, Box } from '@material-ui/core'
+import { useEffect } from 'react'
+import { useNavigate } from 'react-router-dom'
 
 import './home.scss'
 // import skey from '../img/skey.png';
 
 const Home = () => {
+  const navigate = useNavigate()
+  useEffect(() => {
+    setTimeout(() => {
+      navigate('/societykey')
+    }, 2 * 1000) //30 sec idle time
+  }, [])
   return (
     // <Box className='home-total'>
     //   <Container className='home-middle'>
