396bc15e77bc3e920972f4573581ecdd4470b6e1
devninja309 <ryooyama.dev@gmail.com>
2022-08-22 22:34:23 +0900

    improve address

--- src/global/constants.ts
+++ src/global/constants.ts
@@ -2,6 +2,6 @@ import { BigNumber } from '@ethersproject/bignumber'
 
 export const BIG_ZERO = BigNumber.from('0')
 
-export const SocietyNobleContract = '0x2beac1F4A0E0C516F484FA675E4755ce180eF78f'
-export const SocietyCoinContract = '0x8029fBe833886c1F247280255730bca659f8Ee39'
-export const SocietyKeyContract = '0xA864335F7Da39B5Ef0d1cf65C2b8956A75E539d6'
+export const SocietyNobleContract = '0x51437E5BbEA2Ff527F8E76d89aCa03ef49697698'
+export const SocietyCoinContract = '0x1B88E9F152F3384fBecc6BDD0c09b443dd3A2C2D'
+export const SocietyKeyContract = '0x25b21303943D125843176c8bB167b0F6DB18B5B3'
