8ecbd3dd028e2e9ddcec22d06a11d5ab0c515300
Holycitysociety <99046407+Holycitysociety@users.noreply.github.com>
2023-02-08 13:59:00 -0500

    Update constants.ts

--- src/global/constants.ts
+++ src/global/constants.ts
@@ -2,9 +2,9 @@ import { BigNumber } from '@ethersproject/bignumber'
 
 export const BIG_ZERO = BigNumber.from('0')
 
-export const SocietyNobleContract = '0xf3a548fc497028dd6cF14d2A2F9024ADb9005886'
-export const SocietyCoinContract = '0xD096a8Efa2fAC3553B8F3f4Dd25d41b5EcCE070d'
-export const SocietyKeyContract = '0x62e33f583e5Ac27781e59da9260fcbF47a25367F'
+export const SocietyNobleContract = '0x48E709830383905776406F9f34d81Ce76c9A77A6'
+export const SocietyCoinContract = '0xA9aB61a5279365A00627754a4f35F539023AB1f1'
+export const SocietyKeyContract = '0x8EA2B5dedc8aE2232C95eB36A46665F7765e3C23'
 
 // export const SocietyNobleContract = '0x84BEDa4a2Ab31E7421B267d2723b4B375A613714'
 // export const SocietyCoinContract = '0x38Cf1809d6397D7081c896Ccea10949c3b96514C'
